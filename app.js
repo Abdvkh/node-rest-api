38e8d831ebbdf993f99cd58fb684140188a8c4f9
abdvkh <e-address@bk.ru>
2021-01-07 15:26:51 +0500

    Implement posts endpoint with Post model and Create Retrieve features

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv/config');
+
+app.use(bodyParser.json());
+
+//import routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.send('We are on home');
+});
 
 //db connection
 mongoose.connect(
