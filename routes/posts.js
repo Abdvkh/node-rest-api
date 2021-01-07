90296df963887c2e3dea71c4c75dcbe2303f0014
abdvkh <e-address@bk.ru>
2021-01-07 15:54:41 +0500

    Implement full CRUD

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
+//list posts
 router.get('/', async (req, res) => {
     try {
         const posts = await Post.find();
@@ -11,6 +12,7 @@ router.get('/', async (req, res) => {
     }
 });
 
+//create post
 router.post('/', async (req, res) => {
     const post = new Post({
         title: req.body.title,
@@ -25,5 +27,39 @@ router.post('/', async (req, res) => {
     }
 
 });
+
+//retrieve post
+router.get('/:postId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+//delete post
+router.delete('/:postId', async (req, res) => {
+    try {
+        const removedPost = await Post.remove({_id: req.params.postId});
+        res.json(removedPost);
+    } catch (err) {
+        res.json({message: err});
+    }
+});
+
+//update post
+router.patch('/:postId', async (req, res) => {
+    try {
+        const updatedPost = await Post.updateOne(
+            {_id: req.params.postId},
+            {$set: {title: req.body.title}}
+        );
+        res.json(updatedPost);
+    } catch (err) {
+        res.json({message: err});
+    }
+});
+
 
 module.exports = router;
